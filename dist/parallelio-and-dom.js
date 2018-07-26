dcf4f2cf6a57dfed70e93f2838b61a2ac0ef2a0d
Kevin Giguere <kev.giguere.l@gmail.com>
2018-07-26 18:36:23 -0400

    Update agent-base, circular-json, escodegen, follow-redirects, socks-proxy-agent, proxy-agent, commander, wordwrap, colors, gulp-rename, log4js, karma, spark-auto-commit, uglify-js and gulp-uglify

--- dist/parallelio-and-dom.js
+++ dist/parallelio-and-dom.js
@@ -1740,6 +1740,10 @@
         }
       };
 
+      Members.prototype.getValueRef = function(name, obj) {
+        return this.findByRef(name, obj).ref.val;
+      };
+
       Members.prototype.addFunctionRef = function(fn, name, obj) {
         if (this.findRefIndex(name, obj) === -1) {
           fn.ref = {
@@ -1748,6 +1752,10 @@
           };
           return this.push(fn);
         }
+      };
+
+      Members.prototype.findByRef = function(name, obj) {
+        return this._array[this.findRefIndex(name, obj)];
       };
 
       Members.prototype.findRefIndex = function(name, obj) {
@@ -4396,121 +4404,6 @@
   });
 
   (function(definition) {
-    Parallelio.Weapon = definition();
-    return Parallelio.Weapon.definition = definition;
-  })(function(dependencies) {
-    var Damageable, Projectile, Tiled, Timing, Weapon;
-    if (dependencies == null) {
-      dependencies = {};
-    }
-    Tiled = dependencies.hasOwnProperty("Tiled") ? dependencies.Tiled : Parallelio.Tiled;
-    Timing = dependencies.hasOwnProperty("Timing") ? dependencies.Timing : Parallelio.Timing;
-    Damageable = dependencies.hasOwnProperty("Damageable") ? dependencies.Damageable : Parallelio.Damageable;
-    Projectile = dependencies.hasOwnProperty("Projectile") ? dependencies.Projectile : Parallelio.Projectile;
-    Weapon = (function(superClass) {
-      extend(Weapon, superClass);
-
-      Weapon.extend(Damageable);
-
-      function Weapon(options) {
-        this.setProperties(options);
-        Weapon.__super__.constructor.call(this);
-      }
-
-      Weapon.properties({
-        rechargeTime: {
-          "default": 1000
-        },
-        power: {
-          "default": 10
-        },
-        blastRange: {
-          "default": 1
-        },
-        propagationType: {
-          "default": null
-        },
-        projectileSpeed: {
-          "default": 10
-        },
-        target: {
-          "default": null,
-          change: function() {
-            if (this.autoFire) {
-              return this.fire();
-            }
-          }
-        },
-        charged: {
-          "default": true
-        },
-        charging: {
-          "default": true
-        },
-        enabled: {
-          "default": true
-        },
-        autoFire: {
-          "default": true
-        },
-        criticalHealth: {
-          "default": 0.3
-        },
-        canFire: {
-          get: function() {
-            return this.target && this.enabled && this.charged && this.health / this.maxHealth >= this.criticalHealth;
-          }
-        },
-        timing: {
-          calcul: function() {
-            return new Timing();
-          }
-        }
-      });
-
-      Weapon.prototype.fire = function() {
-        var projectile;
-        if (this.canFire) {
-          projectile = new Projectile({
-            origin: this,
-            target: this.target,
-            power: this.power,
-            blastRange: this.blastRange,
-            propagationType: this.propagationType,
-            speed: this.projectileSpeed,
-            timing: this.timing
-          });
-          projectile.launch();
-          this.charged = false;
-          this.recharge();
-          return projectile;
-        }
-      };
-
-      Weapon.prototype.recharge = function() {
-        this.charging = true;
-        return this.chargeTimeout = this.timing.setTimeout((function(_this) {
-          return function() {
-            _this.charging = false;
-            return _this.recharged();
-          };
-        })(this), this.rechargeTime);
-      };
-
-      Weapon.prototype.recharged = function() {
-        this.charged = true;
-        if (this.autoFire) {
-          return this.fire();
-        }
-      };
-
-      return Weapon;
-
-    })(Tiled);
-    return Weapon;
-  });
-
-  (function(definition) {
     Parallelio.SignalOperation = definition();
     return Parallelio.SignalOperation.definition = definition;
   })(function(dependencies) {
@@ -4981,6 +4874,121 @@
 
     })(Tiled);
     return Wire;
+  });
+
+  (function(definition) {
+    Parallelio.Weapon = definition();
+    return Parallelio.Weapon.definition = definition;
+  })(function(dependencies) {
+    var Damageable, Projectile, Tiled, Timing, Weapon;
+    if (dependencies == null) {
+      dependencies = {};
+    }
+    Tiled = dependencies.hasOwnProperty("Tiled") ? dependencies.Tiled : Parallelio.Tiled;
+    Timing = dependencies.hasOwnProperty("Timing") ? dependencies.Timing : Parallelio.Timing;
+    Damageable = dependencies.hasOwnProperty("Damageable") ? dependencies.Damageable : Parallelio.Damageable;
+    Projectile = dependencies.hasOwnProperty("Projectile") ? dependencies.Projectile : Parallelio.Projectile;
+    Weapon = (function(superClass) {
+      extend(Weapon, superClass);
+
+      Weapon.extend(Damageable);
+
+      function Weapon(options) {
+        this.setProperties(options);
+        Weapon.__super__.constructor.call(this);
+      }
+
+      Weapon.properties({
+        rechargeTime: {
+          "default": 1000
+        },
+        power: {
+          "default": 10
+        },
+        blastRange: {
+          "default": 1
+        },
+        propagationType: {
+          "default": null
+        },
+        projectileSpeed: {
+          "default": 10
+        },
+        target: {
+          "default": null,
+          change: function() {
+            if (this.autoFire) {
+              return this.fire();
+            }
+          }
+        },
+        charged: {
+          "default": true
+        },
+        charging: {
+          "default": true
+        },
+        enabled: {
+          "default": true
+        },
+        autoFire: {
+          "default": true
+        },
+        criticalHealth: {
+          "default": 0.3
+        },
+        canFire: {
+          get: function() {
+            return this.target && this.enabled && this.charged && this.health / this.maxHealth >= this.criticalHealth;
+          }
+        },
+        timing: {
+          calcul: function() {
+            return new Timing();
+          }
+        }
+      });
+
+      Weapon.prototype.fire = function() {
+        var projectile;
+        if (this.canFire) {
+          projectile = new Projectile({
+            origin: this,
+            target: this.target,
+            power: this.power,
+            blastRange: this.blastRange,
+            propagationType: this.propagationType,
+            speed: this.projectileSpeed,
+            timing: this.timing
+          });
+          projectile.launch();
+          this.charged = false;
+          this.recharge();
+          return projectile;
+        }
+      };
+
+      Weapon.prototype.recharge = function() {
+        this.charging = true;
+        return this.chargeTimeout = this.timing.setTimeout((function(_this) {
+          return function() {
+            _this.charging = false;
+            return _this.recharged();
+          };
+        })(this), this.rechargeTime);
+      };
+
+      Weapon.prototype.recharged = function() {
+        this.charged = true;
+        if (this.autoFire) {
+          return this.fire();
+        }
+      };
+
+      return Weapon;
+
+    })(Tiled);
+    return Weapon;
   });
 
 }).call(this);
